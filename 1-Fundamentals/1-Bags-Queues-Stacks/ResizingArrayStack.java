b5f8b9a549cf0870f07d68425bc2a9ba8c0d5978
jkim0120 <joskim0120@gmail.com>
2016-05-19 17:43:54 -0400

    Implement ResizingArrayStack

---
+++ 1-Fundamentals/1-Bags-Queues-Stacks/ResizingArrayStack.java
@@ -0,0 +1,84 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ResizingArrayStack<Item> implements Iterable<Item> {
+  private Item[] stack = (Item[]) new Object[1];
+  private int counter = 0;
+
+  public boolean isEmpty() {
+    return counter == 0;
+  }
+
+  public int size() {
+    return counter;
+  }
+
+  private void resize(int max) {
+    Item[] temp = (Item[]) new Object[max];
+
+    for (int i = 0; i < counter; i++)
+      temp[i] = stack[i];
+
+    stack = temp;
+  }
+
+  public void push(Item item) {
+    if (counter == stack.length)
+      resize(2 * stack.length);
+    stack[counter++] = item;
+  }
+
+  public Item pop() {
+    if (isEmpty()) throw new NoSuchElementException("Stack is empty!");
+
+    Item item = stack[--counter];
+    stack[counter] = null;
+
+    if (counter > 0 && counter < stack.length / 4)
+      resize(stack.length / 2);
+
+    return item;
+  }
+
+  public Item peek() {
+    if (isEmpty()) throw new NoSuchElementException("Stack is empty!");
+
+    return stack[counter - 1];
+  }
+
+  public Iterator<Item> iterator() {
+    return new ReverseArrayIterator();
+  }
+
+  private class ReverseArrayIterator implements Iterator<Item> {
+    private int i = counter;
+
+    public boolean hasNext() {
+      return i > 0;
+    }
+
+    public Item next() {
+      return stack[--i];
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+
+  // Unit Test
+
+  public static void main(String[] args) {
+    ResizingArrayStack<String> s = new ResizingArrayStack<String>();
+    while (!StdIn.isEmpty()) {
+        String item = StdIn.readString();
+        if (!item.equals("-")) s.push(item);
+        else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+    }
+    StdOut.println("(" + s.size() + " left on stack)");
+  }
+}
+
